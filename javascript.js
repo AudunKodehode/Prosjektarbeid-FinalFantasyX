f1176bb829a9dda1a167c240a40df3f9cdda078e
Magnus-Alexander <kodhodetmagnus@gmail.com>
2023-10-12 13:21:29 +0200

    ghhdd

--- javascript.js
+++ javascript.js
@@ -1,14 +1,25 @@
-const finalFantasyXContainer = document.getElementById("finalFantasyXContainer");
+const gameContainer = document.getElementById("gameContainer");
 
-const FFOutput = async () => {
-    const characterArray = []
+const FFOutput = async (gameName, divID) => {
     const response = await fetch("https://www.moogleapi.com/api/v1/characters")
     const characters = await response.json();
-
+    // <div id="Final Fantasy"></div>
+    // <div></div>
+    // <div></div>
+    // <div></div>
+    // <div></div>
+    // <div></div>
+    // <div></div>
+    // <div></div>
+    // <div></div>
+    // <div></div>
+    // <div></div>
+    // <div></div>
+    // <div></div>
     for (let character of characters) {
-        if (character.origin === "Final Fantasy X") {
+        if (character.origin === gameName) {
             const characterCard = document.createElement("div");
-            characterCard.classList.add("character-card");
+            characterCard.classList.add("${divID}-card")
             characterCard.innerHTML = `
             <h1 class="name">${character.name}<h1>
             <div id="imageWrapper"><img src="${character.pictures[0].url}" alt=""></div>
@@ -24,4 +35,16 @@ const FFOutput = async () => {
     }
 }
 
-FFOutput();
+FFOutput("Final Fantasy", "ffI");
+FFOutput("Final Fantasy II", "ffII");
+FFOutput("Final Fantasy III", "ffIII");
+FFOutput("Final Fantasy IV", "ffIV");
+FFOutput("Final Fantasy V", "ffV");
+FFOutput("Final Fantasy VI", "ffVI");
+FFOutput("Final Fantasy VII", "ffVII");
+FFOutput("Final Fantasy VIII", "ffVIII");
+FFOutput("Final Fantasy IX", "ffIX");
+FFOutput("Final Fantasy X", "ffX");
+FFOutput("Final Fantasy XII", "ffXII");
+FFOutput("Final Fantasy XIII", "ffXIII");
+FFOutput("Final Fantasy XV", "ffXV");
