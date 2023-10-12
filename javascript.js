08476e7be0d8455a556c03a3dba7874561785545
AudunKodehode <142799630+AudunKodehode@users.noreply.github.com>
2023-10-12 14:39:38 +0200

    *

--- javascript.js
+++ javascript.js
@@ -1,14 +1,30 @@
-const finalFantasyXContainer = document.getElementById("finalFantasyXContainer");
+const gameContainer = document.getElementById("gameContainer");
+const gameSelector = document.getElementById("gameSelector");
 
-const FFOutput = async () => {
-    const characterArray = []
+addEventListener("DOMContentLoaded", function () {
+    for (let i = 0; i < gameContainer.children.length; i++) {
+        gameContainer.children[i].style.display = "none";
+    }
+    document.getElementById(gameSelector.value).style.display = "flex";
+    
+    })
+
+gameSelector.addEventListener("change", function () {
+    for (let i = 0; i < gameContainer.children.length; i++) {
+        gameContainer.children[i].style.display = "none";
+    }
+    gameContainer.children[gameSelector.value].style.display = "flex";
+})
+
+const FFOutput = async (gameName, divID) => {
     const response = await fetch("https://www.moogleapi.com/api/v1/characters")
     const characters = await response.json();
-
+    const gameTypeContainer = document.createElement("div");
+    gameTypeContainer.id = `${divID}Container`;
     for (let character of characters) {
-        if (character.origin === "Final Fantasy X") {
+        if (character.origin === gameName) {
             const characterCard = document.createElement("div");
-            characterCard.classList.add("character-card");
+            characterCard.classList.add("${divID}-card")
             characterCard.innerHTML = `
             <h1 class="name">${character.name}<h1>
             <div id="imageWrapper"><img src="${character.pictures[0].url}" alt=""></div>
@@ -18,10 +34,22 @@ const FFOutput = async () => {
             <p class="job">Job: ${character.job}</p>
             <p class="race">Race: ${character.race}</p>
             <p class="description">${character.description}</p>`
-            console.log(character)
-            finalFantasyXContainer.appendChild(characterCard);
+            gameTypeContainer.appendChild(characterCard);
         }
     }
+    gameContainer.appendChild(gameTypeContainer);
 }
 
-FFOutput();
+FFOutput("Final Fantasy", "ff");
+FFOutput("Final Fantasy II", "ffII");
+FFOutput("Final Fantasy III", "ffIII");
+FFOutput("Final Fantasy IV", "ffIV");
+FFOutput("Final Fantasy V", "ffV");
+FFOutput("Final Fantasy VI", "ffVI");
+FFOutput("Final Fantasy VII", "ffVII");
+FFOutput("Final Fantasy VIII", "ffVIII");
+FFOutput("Final Fantasy IX", "ffIX");
+FFOutput("Final Fantasy X", "ffX");
+FFOutput("Final Fantasy XII", "ffXII");
+FFOutput("Final Fantasy XIII", "ffXIII");
+FFOutput("Final Fantasy XV", "ffXV");
