b3101d708ce7ebb7fe8c3dab364208bd8e7fbc44
AudunKodehode <142799630+AudunKodehode@users.noreply.github.com>
2023-10-14 16:36:52 +0200

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -5,15 +5,15 @@ addEventListener("DOMContentLoaded", function () {
     for (let i = 0; i < gameContainer.children.length; i++) {
         gameContainer.children[i].style.display = "none";
     }
-    document.getElementById(gameSelector.value).style.display = "flex";
-
-})
+    document.getElementById(gameSelector.value).style.display = "none";
+    
+    })
 
 gameSelector.addEventListener("change", function () {
     for (let i = 0; i < gameContainer.children.length; i++) {
         gameContainer.children[i].style.display = "none";
     }
-    gameContainer.children[gameSelector.value].style.display = "flex";
+    gameContainer.children[gameSelector.value].style.display = "";
 })
 
 const FFOutput = async (gameName, divID) => {
@@ -40,16 +40,22 @@ const FFOutput = async (gameName, divID) => {
     gameContainer.appendChild(gameTypeContainer);
 }
 
-FFOutput("Final Fantasy", "ffContainer");
-FFOutput("Final Fantasy II", "ffIIContainer");
-FFOutput("Final Fantasy III", "ffIIIContainer");
-FFOutput("Final Fantasy IV", "ffIVContainer");
-FFOutput("Final Fantasy V", "ffVContainer");
-FFOutput("Final Fantasy VI", "ffVIContainer");
-FFOutput("Final Fantasy VII", "ffVIIContainer");
-FFOutput("Final Fantasy VIII", "ffVIIIContainer");
-FFOutput("Final Fantasy IX", "ffIXContainer");
-FFOutput("Final Fantasy X", "ffXContainer");
-FFOutput("Final Fantasy XII", "ffXIIContainer");
-FFOutput("Final Fantasy XIII", "ffXIIIContainer");
-FFOutput("Final Fantasy XV", "ffXVContainer");
+FFOutput("Final Fantasy", "ff");
+FFOutput("Final Fantasy II", "ffII");
+FFOutput("Final Fantasy III", "ffIII");
+FFOutput("Final Fantasy IV", "ffIV");
+FFOutput("Final Fantasy V", "ffV");
+FFOutput("Final Fantasy VI", "ffVI");
+FFOutput("Final Fantasy VII", "ffVII");
+FFOutput("Final Fantasy VIII", "ffVIII");
+FFOutput("Final Fantasy IX", "ffIX");
+FFOutput("Final Fantasy X", "ffX");
+FFOutput("Final Fantasy XII", "ffXII");
+FFOutput("Final Fantasy XIII", "ffXIII");
+FFOutput("Final Fantasy XV", "ffXV");
+
+
+
+
+
+
