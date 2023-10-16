48ad6f263bc8a817020a2d991b6727fe471b206b
Magnus-Alexander <kodhodetmagnus@gmail.com>
2023-10-16 10:47:39 +0200

    .

--- javascript.js
+++ javascript.js
@@ -1,77 +1,83 @@
 const gameContainer = document.getElementById("gameContainer");
+const gameSelector = document.getElementById("gameSelector");
 
+addEventListener("DOMContentLoaded", function () {
+    // initiate();
+})
+function initiate() {
+    for (let i = 0; i < gameContainer.children.length; i++) {
+        gameContainer.children[i].style.display = "none";
+    }
+    document.getElementById(gameSelector.value).style.display = "grid";
+}
+
+
+
+gameSelector.addEventListener("change", function () {
+    for (let i = 0; i < gameContainer.children.length; i++) {
+        gameContainer.children[i].style.display = "none";
+    }
+    gameContainer.children[gameSelector.value].style.display = "";
+})
+let gameCount = 0;
 const FFOutput = async (gameName, divID) => {
+    gameCount++;
+
     const response = await fetch("https://www.moogleapi.com/api/v1/characters")
     const characters = await response.json();
     const gameTypeContainer = document.createElement("div");
     gameTypeContainer.id = `${divID}Container`;
-    <div>
-        <div id="Final Fantasy"></div>
-        <div id="Final Fantasy II"></div>
-        <div id="Final Fantasy III"></div>
-        <div id="Final Fantasy IV"></div>
-        <div id="Final Fantasy V"></div>
-        <div id="Final Fantasy VI"></div>
-        <div id="Final Fantasy VII"></div>
-        <div id="Final Fantasy VIII"></div>
-        <div id="Final Fantasy IX"></div>
-        <div id="Final Fantasy X"></div>
-        <div id="Final Fantasy XII"></div>
-        <div id="Final Fantasy XIII"></div>
-        <div id="Final Fantasy XV"></div>
-    </div>
     for (let character of characters) {
         if (character.origin === gameName) {
             const characterCard = document.createElement("div");
-            characterCard.classList.add("${divID}-card")
-            characterCard.innerHTML = `
-            <h1 class="name">${character.name}<h1>
-            <div id="imageWrapper"><img src="${character.pictures[0].url}" alt=""></div>
-            <p class="age">Age: ${character.age}</p>
-            <p class="gender">Gender: ${character.gender}</p>
-            <p class="height">Height: ${character.height}</p>
-            <p class="job">Job: ${character.job}</p>
-            <p class="race">Race: ${character.race}</p>
-            <p class="description">${character.description}</p>`
-            console.log(character)
+            characterCard.classList.add(`${divID}-card`);
+
+            // Use a try-catch block to handle potential errors when setting the image src
+            try {
+                characterCard.innerHTML = `
+                    <h1 class="name">${character.name}</h1>
+                    <div id="imageWrapper"><img src="${character.pictures[0].url}" alt=""></div>
+                    <p class="age">Age: ${character.age}</p>
+                    <p class="gender">Gender: ${character.gender}</p>
+                    <p class="height">Height: ${character.height}</p>
+                    <p class="job">Job: ${character.job}</p>
+                    <p class="race">Race: ${character.race}</p>
+                    <p class="description">${character.description}</p>`;
+            } catch (error) {
+                // Handle the error here, you can log it or display a default image
+                characterCard.innerHTML = `
+                    <h1 class="name">${character.name}</h1>
+                    <div id="imageWrapper"><img src="/public/default.png" alt=""></div>
+                    <p class="age">Age: ${character.age}</p>
+                    <p class="gender">Gender: ${character.gender}</p>
+                    <p class="height">Height: ${character.height}</p>
+                    <p class="job">Job: ${character.job}</p>
+                    <p class="race">Race: ${character.race}</p>
+                    <p class="description">${character.description}</p>`;
+            }
+
             gameTypeContainer.appendChild(characterCard);
         }
+
     }
     gameContainer.appendChild(gameTypeContainer);
+    if (gameCount == 13) {
+        initiate();
+    }
 }
-// const FFOutput = async (gameName, divID) => {
-//     const response = await fetch("https://www.moogleapi.com/api/v1/characters")
-//     const characters = await response.json();
 
-//     for (let character of characters) {
-//         if (character.origin === gameName) {
-//             const characterCard = document.createElement("div");
-//             characterCard.classList.add("${divID}-card")
-//             characterCard.innerHTML = `
-//             <h1 class="name">${character.name}<h1>
-//             <div id="imageWrapper"><img src="${character.pictures[0].url}" alt=""></div>
-//             <p class="age">Age: ${character.age}</p>
-//             <p class="gender">Gender: ${character.gender}</p>
-//             <p class="height">Height: ${character.height}</p>
-//             <p class="job">Job: ${character.job}</p>
-//             <p class="race">Race: ${character.race}</p>
-//             <p class="description">${character.description}</p>`
-//             console.log(character)
-//             finalFantasyXContainer.appendChild(characterCard);
-//         }
-//     }
-// }
-
-// FFOutput("Final Fantasy", "ffI");
-// FFOutput("Final Fantasy II", "ffII");
-// FFOutput("Final Fantasy III", "ffIII");
-// FFOutput("Final Fantasy IV", "ffIV");
-// FFOutput("Final Fantasy V", "ffV");
-// FFOutput("Final Fantasy VI", "ffVI");
-// FFOutput("Final Fantasy VII", "ffVII");
-// FFOutput("Final Fantasy VIII", "ffVIII");
-// FFOutput("Final Fantasy IX", "ffIX");
-// FFOutput("Final Fantasy X", "ffX");
-// FFOutput("Final Fantasy XII", "ffXII");
-// FFOutput("Final Fantasy XIII", "ffXIII");
-// FFOutput("Final Fantasy XV", "ffXV");
+FFOutput("Final Fantasy", "ff").then(() => {
+    initiate();
+})
+FFOutput("Final Fantasy II", "ffII");
+FFOutput("Final Fantasy III", "ffIII");
+FFOutput("Final Fantasy IV", "ffIV");
+FFOutput("Final Fantasy V", "ffV");
+FFOutput("Final Fantasy VI", "ffVI");
+FFOutput("Final Fantasy VII", "ffVII");
+FFOutput("Final Fantasy VIII", "ffVIII");
+FFOutput("Final Fantasy IX", "ffIX");
+FFOutput("Final Fantasy X", "ffX");
+FFOutput("Final Fantasy XII", "ffXII");
+FFOutput("Final Fantasy XIII", "ffXIII");
+FFOutput("Final Fantasy XV", "ffXV");
