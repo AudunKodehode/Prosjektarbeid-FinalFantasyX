43b592d6f820c4cf10344cdc728b88b8240e5818
AudunKodehode <audunkodehode@gmail.com>
2023-10-16 14:28:15 +0200

    *

--- javascript.js
+++ javascript.js
@@ -20,10 +20,13 @@ gameSelector.addEventListener("change", function () {
     gameContainer.children[gameSelector.value].style.display = "";
 })
 let gameCount = 0;
+let response = "";
 const FFOutput = async (gameName, divID) => {
     gameCount++;
+    if (response == ""){
+        response = await fetch("https://www.moogleapi.com/api/v1/characters")
+    }
 
-    const response = await fetch("https://www.moogleapi.com/api/v1/characters")
     const characters = await response.json();
     const gameTypeContainer = document.createElement("div");
     gameTypeContainer.id = `${divID}Container`;
@@ -34,6 +37,7 @@ const FFOutput = async (gameName, divID) => {
 
             // Use a try-catch block to handle potential errors when setting the image src
             try {
+                characterCard.id = `${divID}-${character.name}`;
                 characterCard.innerHTML = `
                     <h1 class="name">${character.name}</h1>
                     <div id="imageWrapper"><img src="${character.pictures[0].url}" alt=""></div>
@@ -45,6 +49,7 @@ const FFOutput = async (gameName, divID) => {
                     <p class="description">${character.description}</p>`;
             } catch (error) {
                 // Handle the error here, you can log it or display a default image
+                characterCard.id = `${divID}-${character.name}`;
                 characterCard.innerHTML = `
                     <h1 class="name">${character.name}</h1>
                     <div id="imageWrapper"><img src="/public/default.png" alt=""></div>
@@ -58,7 +63,7 @@ const FFOutput = async (gameName, divID) => {
 
             gameTypeContainer.appendChild(characterCard);
         }
-
+        
     }
     gameContainer.appendChild(gameTypeContainer);
     if (gameCount == 13) {
